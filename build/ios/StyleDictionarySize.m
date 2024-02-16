73ef6957f3f0462ad51fd40495dc919590a8c390
Osborne Rodriguez, Hector <hector.a.rodriguez@accenture.com>
2024-02-16 14:30:44 -0500

    fixing npm audit

---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+
+// Do not edit directly
+// Generated on Tue, 14 Nov 2023 20:15:50 GMT
+
+
+#import "StyleDictionarySize.h"
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
